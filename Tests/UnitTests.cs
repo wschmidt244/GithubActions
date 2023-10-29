9c69cb6c68220ecbd71524524a09eb3b6120c541
wschmidt244 <149318184+wschmidt244@users.noreply.github.com>
2023-10-29 10:51:06 -0500

    Update UnitTests.cs
    fixed addition unit test case

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -11,7 +11,7 @@ namespace GithubActionsLab
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(12, Program.Add("6", "7"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
         [Test]
